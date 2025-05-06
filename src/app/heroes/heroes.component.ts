59e95593de7982023aa118409859566c4e7c1aa7
Bitterguy-code <jasonb4cake@gmail.com>
2025-05-06 09:45:57 -0400

    Implemented "Add services"

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
 import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,10 +15,21 @@ import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
   styleUrl: './heroes.component.css'
 })
 
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit{
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 }
---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MessageService } from '../message.service';
+import { NgFor, CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-messages',
+  standalone: true,
+  imports: [NgFor, CommonModule],
+  templateUrl: './messages.component.html',
+  styleUrl: './messages.component.css'
+})
+export class MessagesComponent {
+  constructor(public messageService: MessageService) {}
+}
